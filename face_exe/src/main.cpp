b9b1514753b30712dd1a040da026d0fd10e52fcc
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-06-05 11:54:28 +0530

    comments added

--- face_exe/src/main.cpp
+++ face_exe/src/main.cpp
@@ -2,50 +2,63 @@
 
 int main()
 {    
-	// path to data folder where DNN models and database for face recognition is stored
+	// path to data folder where DNN models and face database for face recognition is stored
 	const std::string data_path = "..\\..\\data";
 
+	// starting frame number
 	unsigned long frame_number = 0;
 
+	// video file path location
 	const std::string video_src = data_path + "//video//test_video.mp4";
+	
+	// display window name
 	const std::string windowName = "Output frame";
 
-	//Create a Face class object
+	// create a Face class object
 	Face faceObject(data_path, true, false, true);
 
-    //Create video capture object and open video source
-    cv::VideoCapture cap(video_src);
-    cv::Mat frame;
+    	// create video capture object and open video source
+   	cv::VideoCapture cap(video_src);
+	
+	// create opencv Mat object
+   	cv::Mat frame;
 
-	//Create a window to display the output frame
+	// create a window to display the output frame 
 	cv::namedWindow(windowName, cv::WINDOW_FREERATIO);
 
-    if (cap.isOpened())
+    if (cap.isOpened()) // check if video capture object is open
     {
         while(1)
         {
-            if(cap.read(frame))
-            {
-
-				faceObject.runFaceRecognition(frame, frame_number);
-				         
-				cv::imshow(windowName, frame);
-				char k = cv::waitKey(1);
-				if (k == 27 || k == 'q')
-				{
-					break;
-				}
-				else if (k == 'p')
-				{
-					cv::waitKey(0);
-				}
-				frame_number++;
-			}
-			else
+		if(cap.read(frame))
+		{
+			// call the runFaceRecognition and start face recognition
+			faceObject.runFaceRecognition(frame, frame_number);
+			
+			// display the video stream
+			cv::imshow(windowName, frame);
+			
+			// display the frame for 1 milisecond 
+			char k = cv::waitKey(1);
+			
+			// if user presses "Esc" or "q" stop displaying the frame and exit the while loop
+			if (k == 27 || k == 'q')
 			{
 				break;
 			}
-        }
+			else if (k == 'p') // if user presses "p" pause the video streaming
+			{
+				cv::waitKey(0);
+			}
+			
+			// increment the frame number
+			frame_number++;
+		}
+		else
+		{
+			break;
+		}
+	}
     }
     return 0;
 }
